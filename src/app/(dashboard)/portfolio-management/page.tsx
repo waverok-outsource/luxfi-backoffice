a9b051ca1a445a2d2bd47cc8b194aa147aa1b78d
Eleyowo Michael <michael.eleyowo@bitpowr.com>
2026-03-06 11:04:38 +0100

    fix build issue

--- src/app/(dashboard)/portfolio-management/page.tsx
+++ src/app/(dashboard)/portfolio-management/page.tsx
@@ -1,10 +1,15 @@
 import { Metadata } from "next";
 import { PortfolioManagementDashboard } from "@/module/dashboard/portfolio-management";
+import { Suspense } from "react";
 
 export const metadata: Metadata = {
   title: "Portfolio Management",
 };
 
 export default function Page() {
-  return <PortfolioManagementDashboard />;
+  return (
+    <Suspense fallback={null}>
+      <PortfolioManagementDashboard />
+    </Suspense>
+  );
 }
